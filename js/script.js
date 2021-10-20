47863ea5b06b00a3fdf2189abaa14caa582f732d
ValerioMik <valerio.avallone@hotmail.com>
2021-10-20 17:24:55 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -5,20 +5,23 @@ Infine scrivi sulla pagina “La password assegnata è: ” nomecognomecolorepre
 
 /* chiedo il nome */
 
-let nome =prompt("inserisci il tuo nome");
+/* let nome =prompt("inserisci il tuo nome"); */
 
 /* chiedo il cognome */
-let cognome =prompt("inserisci il tuo cognome");
+/* let cognome =prompt("inserisci il tuo cognome"); */
 
 /* chiedo il colore preferito */
 
-let colre =prompt("inserisci il tuo colore preferito");
+/* let colre =prompt("inserisci il tuo colore preferito"); */
 
 /* faccio concatenare i tre risultati  */
+  /* faccio generare una pasword "sicurissima" stampata  a schermo  */
 
-document.getElementById("myid").innerHTML = `${nome}${cognome}${colre}21`
+/* document.getElementById("myid").innerHTML = `${nome}${cognome}${colre}21` */
 
-    /* aggiungo il numero 21 e  */
+ 
+let laMiapasworld
 
-    /* faccio generare una pasword "sicurissima" stampata  a schermo  */
+laMiapasworld='valerioavallone21'
 
+document.getElementById("myid").innerHTML = laMiapasworld
